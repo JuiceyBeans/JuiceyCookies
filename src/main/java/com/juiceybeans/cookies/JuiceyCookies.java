87fab78ed9b4c081389316c9a1639cf24bdee1e7
Alyx <75553966+JuiceyBeans@users.noreply.github.com>
2024-06-05 20:15:02 +0100

    Remove event callbacks

--- src/main/java/com/juiceybeans/cookies/JuiceyCookies.java
+++ src/main/java/com/juiceybeans/cookies/JuiceyCookies.java
@@ -1,9 +1,7 @@
 package com.juiceybeans.cookies;
 
-import com.juiceybeans.cookies.event.ModEventHandler;
 import com.juiceybeans.cookies.item.ModItems;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.player.UseItemCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +14,5 @@ public class JuiceyCookies implements ModInitializer {
     public void onInitialize() {
         ModItems.registerModItems();
         //ServerTickEvents.END_WORLD_TICK.register(ModEventHandler::onServerTick);
-        UseItemCallback.EVENT.register(ModEventHandler::onUseItem);
     }
 }
